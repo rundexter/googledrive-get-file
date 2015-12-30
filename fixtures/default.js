f9d249dc001cd841cd127a225b1137d3cb603571
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-30 16:57:27 +0200

    add DEXTER module

--- fixtures/default.js
+++ fixtures/default.js
@@ -62,8 +62,8 @@ module.exports = _.merge({
              * You should update this section with some test input for testing your module
              */
             input: {
-                //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                fileId: '1bNyaBuxFpOrZzdRn29nglaL6UfUYMi85X1yQjykSNqk',
+                'content': 'content'
             }
         }
     }
